55c2bdfeb11800f17409a05825d0727657020bec
oluwatobi1 <akintunlesetobi@gmail.com>
2023-09-19 13:57:42 +0100

    space update

--- src/calendar-wc.styles.ts
+++ src/calendar-wc.styles.ts
@@ -134,7 +134,7 @@ const CalendarStyles = css`
     font-size: 0.75rem;
     width: 14.2857%;
     height: 26.4px;
-      display: flex;
+    display: flex;
     justify-content: center;
     align-items: center;
     cursor: pointer;
